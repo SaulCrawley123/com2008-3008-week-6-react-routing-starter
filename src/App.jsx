75f7b0c73ac31593eeba6240c181e2704939fca3
Saul Crawley <scrawley2@sheffield.ac.uk>
2025-11-15 01:35:49 +0000

    Lab 5

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import { useState } from "react";
+import {useEffect, useState} from "react";
 
-import { Container } from "react-bootstrap";
-import { Routes, useNavigate } from "react-router-dom";
+import {Alert, Container} from "react-bootstrap";
+import { useLocation, useNavigate } from "react-router-dom";
 import axios from "axios";
 
 import AuthedRoutes from "./AuthedRoutes.jsx";
@@ -22,53 +22,92 @@ const App = () => {
     headers: {
       "Authorization": `Bearer ${token}`
     }
-  });
+  }).catch(errorHandler);
 
-  const login = (token, user) => {
-    setToken(token);
-    setUser(user);
-  };
+    const login = (token, user) => {
+        setToken(token);
+        setUser(user);
+        nav("/")
+        localStorage.setItem("token", token);
+        localStorage.setItem("user", JSON.stringify(user))
+    };
 
   const client = {
     getPosts: () => authorisedRequest("/posts", "GET")
       .then(({ data }) => {
         setPosts(data)
       }),
-    getPost: (id) => authorisedRequest(`/posts/${id}`, "GET"),
-    updatePost: (data, id) => authorisedRequest(`/posts/${id}`, "PUT", data),
-    deletePost: (id) => authorisedRequest(`/posts/${id}`, "DELETE"),
-    createPost: (data) => authorisedRequest("/posts", "POST", data)
+      getPost: (id) => authorisedRequest(`/posts/${id}`, "GET"),
+      updatePost: (data, id) => authorisedRequest(`/posts/${id}`, "PUT", data),
+      deletePost: (id) => authorisedRequest(`/posts/${id}`, "DELETE"),
+      createPost: (data) => authorisedRequest("/posts", "POST", data)
       .then(() => {
         client.getPosts()
       }),
-    signup: (data) => axios({
-      method: "POST",
-      url: `${BASE_URL}/auth/signup`,
-      data
-    }).then(({ data }) => login(data.token, data.user)),
-    login: (data) => axios({
-      method: "POST",
-      url: `${BASE_URL}/auth/login`,
-      auth: data,
-    }).then(({ data }) => login(data.token, data.user)),
+      signup: (data) => axios({
+          method: "POST",
+          url: `${BASE_URL}/auth/signup`,
+          data
+      }).then(loginHandler).catch(errorHandler),
+      login: (data) => axios({
+          method: "POST",
+          url: `${BASE_URL}/auth/login`,
+          data
+      }).then(loginHandler).catch(errorHandler),
   };
 
   const nav = useNavigate();
 
-  const logout = () => {
-    setToken("");
-    setUser({});
-    nav("/");
-  };
+    const location = useLocation();
 
-  return (
-    <Container>
-      {token === ""
-        ? <UnauthedRoutes client={client}/>
-        : <AuthedRoutes user={user} posts={posts} client={client} logout={logout}/>
-      }
-    </Container>
-  );
+    useEffect(() => {
+        setErrorMessage("");
+    }, [location.pathname]);
+
+    const logout = () => {
+        setToken("");
+        setUser({});
+        localStorage.removeItem("token");
+        localStorage.removeItem("user");
+        nav("/");
+    };
+
+    const [errorMessage, setErrorMessage] = useState("");
+
+    const errorHandler = (error) => {
+        setErrorMessage(error.response.data.message)
+        if (error.response.data.errors) {
+            return Promise.reject(error.response.data.errors) // this bit means we can chain another catch()
+        }
+    };
+
+    const loginHandler = ({ data }) => {
+        setToken(data.token);
+        setUser(data.user);
+        setErrorMessage("");
+        nav("/")
+        localStorage.setItem("token", data.token);
+        localStorage.setItem("user", JSON.stringify(data.user))
+    };
+
+    useEffect(() => {
+        setToken(localStorage.getItem("token") ?? "");
+        setUser(JSON.parse(localStorage.getItem("user") ?? "{}"));
+    }, []);
+
+    return (
+        <Container>
+            {errorMessage !== "" && (
+                <Alert className="mt-2" variant="danger">
+                    {errorMessage}
+                </Alert>
+            )}
+            {token === ""
+                ? <UnauthedRoutes client={client}/>
+                : <AuthedRoutes user={user} posts={posts} client={client} logout={logout}/>
+            }
+        </Container>
+    );
 };
 
 export default App;
--- src/RegisterForm.jsx
+++ src/RegisterForm.jsx
@@ -2,48 +2,64 @@ import { useRef, useState } from "react";
 
 import { Link, useNavigate } from "react-router-dom";
 import { Alert, Button, Form } from "react-bootstrap";
+import { isEmpty } from "lodash";
 
 const RegisterForm = ({ client }) => {
   const nav = useNavigate();
 
+  const [errors, setErrors] = useState({})
+
   const usernameRef = useRef(null);
   const passwordRef = useRef(null);
 
-  const [hasError, setHasError] = useState(false);
+    const submitHandler = (e) => {
+        e.preventDefault();
 
-  const submitHandler = (e) => {
-    e.preventDefault();
+        const username = usernameRef.current.value;
+        const password = passwordRef.current.value;
 
-    const username = usernameRef.current.value;
-    const password = passwordRef.current.value;
+        client.signup({ username, password })
+            .catch((errors) => setErrors(errors)) // We're catching the Promise.reject(error.response.data.errors) here
+        // i.e. errors === error.response.data.errors
+    }
 
-    client.signup({ username, password })
-      .then(() => nav("/"))
-      .catch(() => setHasError(true))
-  }
-
-  return (
-    <Form className="mt-2" onSubmit={submitHandler}>
-      <h1>Sign Up</h1>
-      <p>Already have an account? Click <Link to={"/"}>here</Link> to login</p>
-      {hasError && (
-        <Alert variant="danger">
-          An error occurred (this is an unhelpful message that we shall improve later)
-        </Alert>
-      )}
-      <Form.Group controlId="username">
-        <Form.Label>Username</Form.Label>
-        <Form.Control ref={usernameRef} type="text" placeholder="Enter your new username" />
-      </Form.Group>
-      <Form.Group controlId="password">
-        <Form.Label>Password</Form.Label>
-        <Form.Control ref={passwordRef} type="password" placeholder="Enter a strong password" />
-      </Form.Group>
-      <Button className="mt-2" variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
-  );
+    return (
+        <Form className="mt-2" onSubmit={submitHandler}>
+            <h1>Sign Up</h1>
+            <p>Already have an account? Click <Link to={"/"}>here</Link> to login</p>
+            <Form.Group controlId="username">
+                <Form.Label>Username</Form.Label>
+                <Form.Control
+                    isInvalid={!isEmpty(errors.username)} // we need to set this prop
+                    ref={usernameRef}
+                    type="text"
+                    placeholder="Enter your new username"
+                />
+                {!isEmpty(errors?.username) && ( // here is the conditional render
+                    <Form.Control.Feedback type="invalid">
+                        {errors.username.join(", ")}
+                    </Form.Control.Feedback>
+                )}
+            </Form.Group>
+            <Form.Group controlId="password">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                    isInvalid={!isEmpty(errors.password)} // we need to set this prop
+                    ref={passwordRef}
+                    type="password"
+                    placeholder="Enter a strong password"
+                />
+                {!isEmpty(errors?.password) && ( // here is the conditional render
+                    <Form.Control.Feedback type="invalid">
+                        {errors.password.join(", ")}
+                    </Form.Control.Feedback>
+                )}
+            </Form.Group>
+            <Button className="mt-2" variant="primary" type="submit">
+                Submit
+            </Button>
+        </Form>
+    );
 };
 
 export default RegisterForm;
